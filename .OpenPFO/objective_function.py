4d5948e385fb352a778892c3d4387f159c954dfe
Thomas Kim <thomas@you.com>
2025-10-27 10:20:27 -0400

    Add OpenPFO template files

--- .OpenPFO/objective_function.py
+++ .OpenPFO/objective_function.py
@@ -7,8 +7,19 @@ def objective_function(something) -> float:
     The objective function MUST process OpenFOAM results for each grid point in
     the design space using a post-processor of your choice.
 
-    This objective function MUST return a single floating point value, the units
-    of the value do not matter as the comparison between grid points in the
-    design space are relative.
+    This objective function MUST return a single floating point value
+    (objective_value), please reassign this value in your code. The units of the
+    value do not matter as the comparison between grid points in the design
+    space are relative.
+
+    Optionally, you can add side effects to your optimization such as image
+    extractation and data analysis using inputs/side_effects.py
     """
-    pass
+
+    objective_value = 0
+
+    """ ======================= YOUR CODE BELOW HERE ======================= """
+
+    """ ======================= YOUR CODE ABOVE HERE ======================= """
+
+    return objective_value
---
+++ .OpenPFO/side_effects.py
@@ -0,0 +1,19 @@
+def side_effects(side_effects_directory: str) -> None:
+    """
+    The side effects function is used to add side effects to your optimization
+    workflow such as image extractation and data analysis. The side effects
+    function runs AFTER the objective function is ran for each grid point in the
+    design space.
+
+    All data from this function will be available to export after the full
+    workflow has completed. You may store any data from this function in the
+    provided side effects output directory.
+
+    This function does not return a value.
+    """
+
+    """ ======================= YOUR CODE BELOW HERE ======================= """
+
+    """ ======================= YOUR CODE ABOVE HERE ======================= """
+
+    return None
