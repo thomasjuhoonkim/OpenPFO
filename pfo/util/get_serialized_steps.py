537982d3f934d435881d6abceab84a159992802c
Thomas Kim <thomasjuhoonkim@gmail.com>
2026-01-19 02:10:01 -0500

    Fixes

--- pfo/util/get_serialized_steps.py
+++ pfo/util/get_serialized_steps.py
@@ -5,7 +5,7 @@ from classes.step import Step
 def get_serialized_steps(steps: list[Step]):
     return [
         {
-            "id": step.get_id(),
+            "id": step.get_id().value,
             "runOk": step.get_run_ok(),
             "startTime": step.get_start_time().isoformat(),
             "endTime": step.get_end_time().isoformat(),
