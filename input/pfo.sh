c66eb10d82b4347f8507276a2f4c801e0b37c1ed
Thomas Kim <thomasjuhoonkim@gmail.com>
2026-01-18 20:38:27 -0500

    Reduce to 32 cores, increase to 3 jobs

--- input/pfo.sh
+++ input/pfo.sh
@@ -2,7 +2,7 @@
 
 #SBATCH --job-name=pfo
 #SBATCH --account=def-jphickey
-#SBATCH --time=01:00:00
+#SBATCH --time=03:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
@@ -14,4 +14,4 @@ module load python 3.11
 module load openfoam/v2312
 module load paraview/6.0.0
 
-pfo checkRun --count=10
+pfo checkRun --count=3
