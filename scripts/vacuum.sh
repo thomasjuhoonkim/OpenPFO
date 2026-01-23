b6754e3f6630527899a7f12cdb51407b868a0924
Thomas Kim <thomas@you.com>
2026-01-23 11:33:45 -0500

    Add vacuum script

---
+++ scripts/vacuum.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+echo "Deleting processor* directories"
+find ./output/cases -type d -name "processor*" -exec rm -rf {} +
+
+echo "Deleting timestep directories"
+find ./output/cases -type d -regextype posix-extended -regex "./[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?" -exec rm -rf {} +
