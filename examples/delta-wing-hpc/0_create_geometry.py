bc3841fbf481e69e7795a106aa93ac65a3327ea2
Thomas Kim <thomas@you.com>
2026-01-25 17:26:40 -0500

    Save hpc progress

---
+++ examples/delta-wing-hpc/0_create_geometry.py
@@ -0,0 +1,84 @@
+# classes
+from classes.functions import CreateGeometryParameters, CreateGeometryReturn
+from classes.modeler import FreeCADModeler, OpenVSPModeler
+from classes.point import Point
+from classes.variable import Variable
+
+
+def create_geometry(
+    create_geometry_parameters: CreateGeometryParameters,
+) -> CreateGeometryReturn:
+    """
+    The create_geometry function is used to create the geometry for each grid
+    point in the design space.
+
+    NOTE: This function returns the geometry filepath.
+    """
+
+    """ ======================= YOUR CODE BELOW HERE ======================= """
+
+    # destructure parameters
+    job_id = create_geometry_parameters.job_id
+    point = create_geometry_parameters.point
+    output_assets_directory = create_geometry_parameters.output_assets_directory
+
+    # OpenVSP
+    OPENVSP_FILEPATH = "/home/tkim/scratch/OpenVSP/build/vsp/vspscript"
+    MODEL_FILEPATH = "input/model.vsp3"
+
+    openvsp_modeler = OpenVSPModeler(
+        model_filepath=MODEL_FILEPATH, openvsp_filepath=OPENVSP_FILEPATH
+    )
+    openvsp_modeler.check_model()
+
+    root_chord_variable = point.get_variables()[0]
+    root_chord = root_chord_variable.get_value()
+    tip_chord = 0.10  # metres
+    taper_ratio = tip_chord / root_chord
+    sweep = 2253.36 * taper_ratio**2 - 730.13 * taper_ratio + 79.72
+    sweep_variable = Variable(
+        name="Sweep", id="DFMNISRTXAJ:WingGeom:XSec_1:Sweep", value=sweep
+    )
+    updated_variables = [*point.get_variables(), sweep_variable]
+    updated_point = Point(job_id=job_id, variables=updated_variables)
+
+    output_geometry_filepath = openvsp_modeler.generate_geometry(
+        job_id=job_id,
+        point=updated_point,
+        output_assets_directory=output_assets_directory,
+    )
+
+    CREATE_GEOMETRY_RETURN = CreateGeometryReturn(
+        output_geometry_filepath=output_geometry_filepath,
+    )
+
+    # ==========================================================================
+
+    # # destructure parameters
+    # point = create_geometry_parameters.point
+    # job_id = create_geometry_parameters.job_id
+    # output_assets_directory = create_geometry_parameters.output_assets_directory
+
+    # FreeCAD
+    # MODEL_FILEPATH = "input/model.FCStd"
+    # FREECAD_FILEPATH = "/Applications/FreeCAD.app/Contents/Resources/lib"
+    # # FREECAD_FILEPATH = "/usr/lib/freecad/lib"
+    # # FREECAD_FILEPATH = "C:\\Program Files\\FreeCAD\\bin"
+    # freecad_modeler = FreeCADModeler(
+    #     model_filepath=MODEL_FILEPATH, freecad_filepath=FREECAD_FILEPATH
+    # )
+    # freecad_modeler.check_model()
+
+    # output_geometry_filepath = freecad_modeler.generate_geometry(
+    #     job_id=job_id,
+    #     point=point,
+    #     output_assets_directory=output_assets_directory,
+    # )
+
+    # CREATE_GEOMETRY_RETURN = CreateGeometryReturn(
+    #     output_geometry_filepath=output_geometry_filepath
+    # )
+
+    """ ======================= YOUR CODE ABOVE HERE ======================= """
+
+    return CREATE_GEOMETRY_RETURN
---
+++ examples/delta-wing-hpc/input/paraview/cp-contour.py
@@ -0,0 +1,66 @@
+import sys
+
+from paraview.simple import *
+
+paraview.simple._DisableFirstRenderCameraReset()
+
+if __name__ == "__main__":
+    # validate inputs
+    if len(sys.argv) < 3:
+        print("Usage: pvbatch -- script.py <input_filepath> <assets_directory>")
+        sys.exit(1)
+
+    # parse inputs
+    input_filepath = sys.argv[1]
+    assets_directory = sys.argv[2]
+
+    # load data
+    reader = OpenDataFile(input_filepath)
+
+    """ ======================= YOUR CODE BELOW HERE ======================= """
+
+    VIEW_SIZE = [3840, 2160]
+
+    renderView = CreateView("RenderView")
+    renderView.ViewSize = VIEW_SIZE
+
+    renderView.CameraPosition = [-1.25, 1.25, 1]
+    renderView.CameraFocalPoint = [0.5, 0, 0]
+    renderView.CameraViewUp = [0, 0, 1]
+
+    parafoam = GetActiveSource()
+    parafoam.MeshRegions = ["patch/jobGeometry"]
+
+    pLUT = GetColorTransferFunction("p")
+    HideScalarBarIfNotNeeded(pLUT, renderView)
+
+    calculator1 = Calculator(registrationName="Calculator1", Input=parafoam)
+
+    calculator1.Set(
+        ResultArrayName="Cp",
+        Function="= (p - 0)/(0.5*1.225*(10^2))",
+    )
+    calculator1.AttributeType = "Cell Data"
+
+    display1 = Show(calculator1, renderView, "GeometryRepresentation")
+    display1.RescaleTransferFunctionToDataRange(True, False)
+    display1.SetScalarBarVisibility(renderView, True)
+    display1.Representation = "Surface"
+
+    Hide(parafoam, renderView)
+
+    ColorBy(display1, ("CELLS", "Cp"))
+
+    cpLUT = GetColorTransferFunction("Cp")
+
+    Render()
+
+    SaveScreenshot(
+        f"{assets_directory}/cp-contour.png",
+        renderView,
+        ImageResolution=renderView.ViewSize,
+    )
+
+    """ ======================= YOUR CODE ABOVE HERE ======================= """
+
+    ResetSession()
