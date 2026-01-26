fc0b0999f0c9164f94204d038e65480e495e9870
Thomas Kim <thomas@you.com>
2026-01-25 21:29:04 -0500

    Add memory

--- pfo/2_create_mesh.py
+++ pfo/2_create_mesh.py
@@ -29,7 +29,7 @@ def create_mesh(
         nodes=1,
         ntasks_per_node=1,
         cpus_per_task=processors_per_job,
-        mem="16G",
+        mem="32G",
         output="OpenPFO.log",
         open_mode="append",
     )
