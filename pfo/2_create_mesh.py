923778e8d8987982d08c56c47819d6a3a56684d5
Thomas Kim <thomas@you.com>
2026-01-26 13:46:50 -0500

    Give cartesianMesh more time

--- pfo/2_create_mesh.py
+++ pfo/2_create_mesh.py
@@ -26,7 +26,7 @@ def create_mesh(
     slurm = Slurm(
         job_name=f"{job_id}-cartesianMesh",
         account="def-jphickey",
-        time="00:10:00",
+        time="00:15:00",
         nodes=1,
         ntasks_per_node=1,
         cpus_per_task=processors_per_job,
