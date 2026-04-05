4019c685e9e504217f2cd8499cd8990a96216e71
Thomas Kim <thomas@you.com>
2026-04-05 14:14:36 -0400

    Fix solutions for single objective optimization

--- pfo/util/get_solutions.py
+++ pfo/util/get_solutions.py
@@ -11,22 +11,38 @@ from constants.objective import ObjectiveType
 from util.get_points import get_point
 from util.get_config_objectives import get_config_objectives
 
+config_objectives = get_config_objectives()
+
 
 def get_solutions(result: Any):
     solutions: list[Solution] = []
 
-    for i in range(len(result.X)):
-        objectives = get_config_objectives()
+    # if this is a single-objective problem, result will simply return one best
+    # point, for multi-objective problems, result will be an list of
+    # non-dominated solutions
+    if len(config_objectives) == 1:
+        point = get_point(coordinates=result.X)
+        objective = get_config_objectives()[0]
+        objective_value = result.F[0]
+        if objective.get_type() == ObjectiveType.MAXIMIZE:
+            objective.set_value(-1 * objective_value)
+        else:
+            objective.set_value(objective_value)
 
-        point = get_point(coordinates=result.X[i])
-        for j, objective_value in enumerate(result.F[i]):
-            config_objective = objectives[j]
-            if config_objective.get_type() == ObjectiveType.MAXIMIZE:
-                objectives[j].set_value(-1 * objective_value)
-            else:
-                objectives[j].set_value(objective_value)
-
-        solution = Solution(point=point, objectives=objectives)
+        solution = Solution(point=point, objectives=[objective])
         solutions.append(solution)
+    else:
+        for i in range(len(result.X)):
+            point = get_point(coordinates=result.X[i])
+            objectives = get_config_objectives()
+            for j, objective_value in enumerate(result.F[i]):
+                objective = objectives[j]
+                if objective.get_type() == ObjectiveType.MAXIMIZE:
+                    objectives[j].set_value(-1 * objective_value)
+                else:
+                    objectives[j].set_value(objective_value)
+
+            solution = Solution(point=point, objectives=objectives)
+            solutions.append(solution)
 
     return solutions
